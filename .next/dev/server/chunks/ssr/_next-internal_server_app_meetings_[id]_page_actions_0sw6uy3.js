eba36d0ea47009d658d85f535f2d6ed7942c24fc
De-Coder05 <dwadhwani_be23@thapar.edu>
2026-04-28 15:49:11 +0530

    Clean up junk and add proper gitignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_meetings_[id]_page_actions_0sw6uy3.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/meetings/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_meetings_%5Bid%5D_page_actions_0sw6uy3.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sK21NOm9g3Mo9658fN8OOsTtBf+GBcKnayuDgWzBAzE=\"\n}"
